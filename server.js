dabf5f1482d647a904666f8e1f8c167c0f7a5ec8
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2025-06-11 23:00:09 +0000

    build: Deploy heroku try again

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from public/site
+// Serve static files from 'public/site'
 app.use(express.static(path.join(__dirname, "public/site")));
 
-// Fallback to index.html for all other routes
-app.get("*", (req, res) => {
+// Fallback route for single-page apps or 404s
+app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "public/site", "index.html"));
 });
 
