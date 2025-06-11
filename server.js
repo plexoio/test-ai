d1f997ace91c789a2db36d63e390e5330d03450a
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2025-06-11 23:01:26 +0000

    build: Correct path

--- server.js
+++ server.js
@@ -4,13 +4,8 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from 'public/site'
-app.use(express.static(path.join(__dirname, "public/site")));
-
-// Fallback route for single-page apps or 404s
-app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/site", "index.html"));
-});
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, "public/site/")));
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
